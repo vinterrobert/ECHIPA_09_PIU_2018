cadc38a46f7badabdd0e6f1ded81475e62fff18d
Pusca Priscila <priscila1.maria@gmail.com>
2018-12-10 21:19:01 +0200

    add two buttons in main page

---
+++ js/home.js
@@ -0,0 +1,19 @@
+
+function openMyWallet() {
+
+	window.location = "myWallet.html";
+	return false;
+}
+
+function addNewWallet(){
+
+	alert("Not implemented yet");
+	return false;
+
+}
+
+function addEconomyAccount(){
+
+	alert("Not implemented yet");
+	return false;
+}
