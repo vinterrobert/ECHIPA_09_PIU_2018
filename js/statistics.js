c2a419d8ab7c54587bed4af2710b6cd5b5a56092
Norbert Lorinczi <lorinczi.norbi@gmail.com>
2019-01-14 22:33:45 +0200

    added tabs for my wallet screen

--- js/statistics.js
+++
@@ -1,8 +0,0 @@
-$('.progress-fill span').each(function(){
-  var percent = $(this).html();
-  var pTop = 100 - ( percent.slice(0, percent.length - 1) ) + "%";
-  $(this).parent().css({
-    'height' : percent,
-    'top' : pTop
-  });
-});
